374c02043032cdad51cd85d9d84d479e978fce67
Peter Širka <petersirka@gmail.com>
2021-10-10 10:39:35 +0200

    Improve code.

--- definitions/func.js
+++ definitions/func.js
@@ -43,8 +43,3 @@ FUNC.preparetokens = function() {
 	}
 
 };
-
-ON('ready', function() {
-	PREF.name && LOADCONFIG({ name: PREF.name, allow_tms: PREF.allow_tms, secret_tms: PREF.secret_tms });
-	FUNC.preparetokens();
-});
--- definitions/main.js
+++ definitions/main.js
@@ -2,5 +2,7 @@ MAIN.version = 1;
 MAIN.stats = MEMORIZE('stats');
 MAIN.tokens = {};
 
-if (PREF.name)
-	CONF.name = PREF.name;
+ON('ready', function() {
+	PREF.name && LOADCONFIG({ name: PREF.name, allow_tms: PREF.allow_tms, secret_tms: PREF.secret_tms });
+	FUNC.preparetokens();
+});
