2332398fcdbda3575079c20bffcb754b1af5bba6
Peter Širka <petersirka@gmail.com>
2021-10-12 09:32:13 +0200

    Added missing function.

--- definitions/func.js
+++ definitions/func.js
@@ -43,3 +43,10 @@ FUNC.preparetokens = function() {
 	}
 
 };
+
+FUNC.saveconfig = function() {
+	var config = {};
+	for (var item of F.extensions)
+		config[item.id] = item.config;
+	F.Fs.writeFile(PATH.databases('extensions.json'), JSON.stringify(config), NOOP);
+};
